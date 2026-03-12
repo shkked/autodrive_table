96f0a0ddc654f0296411c142177658d972ab31e7
kenny <pletnev735@gmail.com>
2026-03-13 00:08:16 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite"
+import vue from "@vitejs/plugin-vue"
+import { fileURLToPath, URL } from "url"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  base: '/autodrive_table/',
+	plugins: [vue(), tailwindcss()],
+	base: "/autodrive_table/",
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 })
