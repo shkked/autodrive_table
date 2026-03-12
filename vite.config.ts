65cbd1ad3522d381cde49e53208a02d6a3228cb2
kenny <pletnev735@gmail.com>
2026-03-13 00:38:32 +0300

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue(), tailwindcss()],
-	base: "/autodrive_table/",
+	// base: "/autodrive_table/",
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
